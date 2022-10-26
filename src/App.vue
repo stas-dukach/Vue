d78dc456152abdb0cb1629fd8b916570ee5c3c94
Stas Dukach <stasdukach@gmail.com>
2022-10-26 13:00:36 +0300

    x

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <h1>Parent {{ pizzaName }}</h1>
+    <app-pizza :pizzaName = 'pizzaName' :pizzaPrice="pizzaPrice" :pizzaSize="pizzaSize" @nameChanged = 'pizzaName = $event'> </app-pizza>
+    <appCounter ></appCounter>
+  </div>
+</template>
+
+<script>
+import Pizza from './Pizza.vue'
+import Counter from './Counter.vue'
+export default {
+  name: 'app',
+  data () {
+    return {
+      pizzaName: 'Dominos',
+      pizzaPrice: "ddd",
+    }
+  },
+  components: {
+    appPizza: Pizza,
+    appCounter: Counter
+  }
+}
+</script>
+
